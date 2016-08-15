294a0b67ceae0c089004e476bfb200edd06bc170
Robin Valk <rjmvalk3@gmail.com>
2016-08-15 10:47:50 +0200

    Updated credentials

---
+++ tests/Message/PayPalPurchaseRequestTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Omnipay\Novalnet\Message;
+
+use Omnipay\Novalnet\Gateway;
+
+class PayPalPurchaseRequestTest extends AbstractPurchaseRequestTest {
+    protected function getRequest()
+    {
+        return new PurchaseRequestPayPal($this->getHttpClient(), $this->getHttpRequest());
+    }
+
+    protected function getPaymentMethod()
+    {
+        return Gateway::PAYPAL_METHOD;
+    }
+
+    protected function getRedirectUrl()
+    {
+        return 'https://payport.novalnet.de/paypal_payport';
+    }
+}
