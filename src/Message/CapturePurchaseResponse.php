add7b5266ac9fd5207420ef86ce48ef45df30ea7
Manuel <ps3udonym4@googlemail.com>
2018-03-11 10:07:03 +0100

    Fixed property of non object error when acessing data array in CapturePuchaseResponse

--- src/Message/CapturePurchaseResponse.php
+++ src/Message/CapturePurchaseResponse.php
@@ -5,4 +5,13 @@ namespace Omnipay\Novalnet\Message;
 class CapturePurchaseResponse extends AbstractResponse
 {
 
+    public function getCode()
+    {
+        return $this->data['status'];
+    }
+
+    public function getMessage()
+    {
+        return $this->data['status_desc'];
+    }
 }
